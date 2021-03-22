077a086afb38728e53a1bcefde5f01596a74fb04
DMkelllog <kelllog@naver.com>
2021-03-22 15:31:24 +0900

    Initial commit

--- README.md
+++ README.md
@@ -40,7 +40,6 @@ Wafer map defect pattern classification using manual feature extraction
 ## References
 
 * WM-811K(LSWMD). National Taiwan University Department of Computer Science Multimedia Information Retrieval LAB http://mirlab.org/dataSet/public/
-* Nakazawa, T., & Kulkarni, D. V. (2018). Wafer map defect pattern classification and image retrieval using convolutional neural network. IEEE Transactions on Semiconductor Manufacturing, 31(2), 309-314.
-* Shim, J., Kang, S., & Cho, S. (2020). Active learning of convolutional neural network for cost-effective wafer map pattern classification. IEEE Transactions on Semiconductor Manufacturing, 33(2), 258-266.
-* Kang, S. (2020). Rotation-Invariant Wafer Map Pattern Classification With Convolutional Neural Networks. IEEE Access, 8, 170650-170658.
-
+* Wu, M. J., Jang, J. S. R., & Chen, J. L. (2014). Wafer map failure pattern recognition and similarity ranking for large-scale data sets. IEEE Transactions on Semiconductor Manufacturing, 28(1), 1-12.
+* Fan, M., Wang, Q., & van der Waal, B. (2016, October). Wafer defect patterns recognition based on OPTICS and multi-label classification. In 2016 IEEE Advanced Information Management, Communicates, Electronic and Automation Control Conference (IMCEC) (pp. 912-915). IEEE.
+* Saqlain, M., Jargalsaikhan, B., & Lee, J. Y. (2019). A voting ensemble classifier for wafer map defect patterns identification in semiconductor manufacturing. IEEE Transactions on Semiconductor Manufacturing, 32(2), 171-182.
--- run_code/run_MFE.py
+++ run_code/run_MFE.py
@@ -1,4 +1,7 @@
-# Wafer map pattern classification using CNN
+# Wafer map pattern classification using MFE
+
+import pickle
+import os
 
 import pickle
 import os
@@ -6,9 +9,8 @@ import os
 import numpy as np
 import tensorflow as tf
 from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-
-from tensorflow.keras.applications.vgg16 import VGG16
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
@@ -17,7 +19,6 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-DIM = 64
 BATCH_SIZE = 32
 MAX_EPOCH = 1000
 TRAIN_SIZE_LIST = [500, 5000, 50000, 162946]
@@ -25,22 +26,29 @@ LEARNING_RATE = 1e-4
 
 early_stopping = tf.keras.callbacks.EarlyStopping(patience=20, restore_best_weights=True)
 
-with open('../data/X_CNN_64.pickle', 'rb') as f:
-    X_resize = pickle.load(f)
+with open('../data/X_MFE.pickle', 'rb') as f:
+    X_mfe = pickle.load(f)
 with open('../data/y.pickle', 'rb') as f:
     y = pickle.load(f)
     
-# Stack wafer maps as 3 channels to correspond with RGB channels.
-X_resize = (X_resize - 0.5) * 2
-X_resize_stacked = np.repeat(X_resize, 3, -1)
-y_onehot = tf.keras.utils.to_categorical(y)
+def FNN(lr=1e-4):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(128, activation='tanh'),
+        tf.keras.layers.Dense(128, activation='tanh'),
+        tf.keras.layers.Dense(9, activation='softmax')
+    ])
+    model.compile(optimizer= tf.keras.optimizers.Adam(lr=lr),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
 
 REP_ID = 0
-RAN_NUM = 27407 + REP_ID
+RAN_NUM = 27407+REP_ID
 
 for TRAIN_SIZE_ID in range(4):
     TRAIN_SIZE = TRAIN_SIZE_LIST[TRAIN_SIZE_ID]
-    X_trnval, X_tst, y_trnval, y_tst =  train_test_split(X_resize_stacked, y_onehot, 
+
+    X_trnval, X_tst, y_trnval, y_tst =  train_test_split(X_mfe, y, 
                                                          test_size=10000, random_state=RAN_NUM)
 
     # Randomly sample train set for evaluation at various training set size
@@ -49,29 +57,28 @@ for TRAIN_SIZE_ID in range(4):
     else:
         X_trnval,_ , y_trnval, _ = train_test_split(X_trnval, y_trnval, 
                                                     train_size=TRAIN_SIZE, random_state=RAN_NUM)
-
     # Get unique labels in training set. Some labels might not appear in small training set.
-    labels = np.unique(np.argmax(y_trnval, 1))
+    labels = np.unique(y_trnval)
 
-    base_model = VGG16(weights=None, pooling='avg', include_top=False)
-    predictions = tf.keras.layers.Dense(9, activation='softmax')(base_model.output)
-    model = tf.keras.Model(inputs=base_model.input, outputs=predictions)
-    for layer in base_model.layers: layer.trainable = True
-    model.compile(optimizer= tf.keras.optimizers.Adam(lr=LEARNING_RATE), 
-                  loss='categorical_crossentropy', 
-                  metrics=['accuracy'])
+    scaler = StandardScaler()
+    X_trnval_scaled = scaler.fit_transform(X_trnval)
+    X_tst_scaled = scaler.transform(X_tst)
 
-    log = model.fit(X_trnval, y_trnval, validation_split=0.2, 
-                    batch_size=BATCH_SIZE, epochs=MAX_EPOCH, 
-                    callbacks=[early_stopping], verbose=0)
-    y_trnval_hat= model.predict(X_trnval)
-    y_tst_hat= model.predict(X_tst)
+
+    model = FNN(lr=LEARNING_RATE)
+
+    y_trnval = tf.keras.utils.to_categorical(y_trnval)
+    y_tst = tf.keras.utils.to_categorical(y_tst)
+    log = model.fit(X_trnval_scaled, y_trnval, validation_split=0.2, batch_size=BATCH_SIZE,
+              epochs=MAX_EPOCH, callbacks=[early_stopping], verbose=0)
+    y_trnval_hat= model.predict(X_trnval_scaled)
+    y_tst_hat= model.predict(X_tst_scaled)
 
     macro = f1_score(np.argmax(y_tst, 1), np.argmax(y_tst_hat, 1), labels=labels, average='macro')
     micro = f1_score(np.argmax(y_tst, 1), np.argmax(y_tst_hat, 1), labels=labels, average='micro')
     cm = confusion_matrix(np.argmax(y_tst, 1), np.argmax(y_tst_hat, 1))
 
-    filename = '../result/CNN/WMPC_'+'CNN_'+'_'+str(TRAIN_SIZE)+'_'+str(REP_ID)+'_'
+    filename = '../result/WMPC_'+'MFE_'+str(TRAIN_SIZE)+'_'+str(REP_ID)+'_'
 
     with open(filename+'softmax.pickle', 'wb') as f:
         pickle.dump([y_trnval_hat, y_tst_hat], f)
